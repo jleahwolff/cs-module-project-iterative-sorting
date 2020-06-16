a55c206d8c726284c477b76654fd6f6366e031a1
Jessica Wolff <jleahwolff@gmail.com>
2020-06-16 14:30:20 -0500

    Done. Cant figure out Binary Search

--- src/searching/searching.py
+++ src/searching/searching.py
@@ -1,6 +1,8 @@
 def linear_search(arr, target):
     # Your code here
-
+    for i in range(len(arr)):
+        if (arr[i] == target):
+            return i
 
     return -1   # not found
 
@@ -8,7 +10,22 @@ def linear_search(arr, target):
 # Write an iterative implementation of Binary Search
 def binary_search(arr, target):
 
+    left = target -1
+    right = target + 1
     # Your code here
+    if right >= left:
+        mid = left + (right - 1)
+
+        if arr[mid] == target:
+            return mid
+        
+        elif arr[mid] > target:
+            return binary_search(arr, left, mid-1, target)
+        
+        else:
+            return binary_search(arr, mid + 1, right, target)
+
+    else:
+        return - 1
 
 
-    return -1  # not found
